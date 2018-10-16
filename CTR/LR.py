f82ace2ac0bd0c00cf9f25a3ba18171d99ec6d5a
Hourout <hourout@163.com>
2018-10-16 18:03:10 +0800

    Update LR.py

--- CTR/LR.py
+++ CTR/LR.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 
 def lr_model():
-    inputs = tf.keras.Input((100,))
+    inputs = tf.keras.Input((30,))
     pred = tf.keras.layers.Dense(units=1, 
                                  bias_regularizer=tf.keras.regularizers.l2(0.01),
                                  kernel_regularizer=tf.keras.regularizers.l1(0.02),
@@ -18,7 +18,8 @@ def lr_model():
 def train():
     lr = lr_model()
     data = load_breast_cancer()
-    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2,
+                                                        random_state=27, stratify=data.target)
     lr.fit(X_train, y_train, epochs=3, batch_size=16, validation_data=(X_test, y_test))
     return lr
 
