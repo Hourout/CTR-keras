92b169a6e27ebbcfe353d4dd59f6bd0db7a955a2
JinQing Lee <hourout@163.com>
2018-12-21 14:42:58 +0800

    add demo

--- CTR/FFM.py
+++ CTR/FFM.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
 K = tf.keras.backend
 
 
 class MyLayer(tf.keras.layers.Layer):
     def __init__(self, field_dict, field_dim, input_dim, output_dim=30, **kwargs):
-        self.filed_dict = field_dict
-        self.filed_dim = field_dim
+        self.field_dict = field_dict
+        self.field_dim = field_dim
         self.input_dim = input_dim
         self.output_dim = output_dim
         super(MyLayer, self).__init__(**kwargs)
@@ -21,8 +23,9 @@ class MyLayer(tf.keras.layers.Layer):
         self.field_cross = K.variable(0, dtype='float32')
         for i in range(self.input_dim):
             for j in range(i+1, self.input_dim):
-                self.field_cross += K.dot(self.kernel[i, self.field_dict[j]],
-                    K.transpose(self.kernel[j, self.field_dict[i]]))*tf.keras.layers.multiply([x[:,i], x[:,j]])
+                weight = tf.math.reduce_sum(tf.math.multiply(self.kernel[i, self.field_dict[j]], self.kernel[j, self.field_dict[i]]))
+                value = tf.math.multiply(weight, tf.math.multiply(x[:,i], x[:,j]))
+                self.field_cross = tf.math.add(self.field_cross, value)
         return self.field_cross
 
     def compute_output_shape(self, input_shape):
@@ -31,13 +34,27 @@ class MyLayer(tf.keras.layers.Layer):
 def FFM(feature_dim, field_dict, field_dim, output_dim=30):
     inputs = tf.keras.Input((feature_dim,))
     liner = tf.keras.layers.Dense(1)(inputs)
+    print(liner.shape)
     cross = MyLayer(field_dict, field_dim, feature_dim, output_dim)(inputs)
+    cross = tf.keras.layers.Reshape((1,))(cross)
+    print(cross.shape)
     add = tf.keras.layers.Add()([liner, cross])
-    predictions = tf.keras.Activation('sigmoid')(add)
+    predictions = tf.keras.layers.Activation('sigmoid')(add)
     model = tf.keras.Model(inputs=inputs, outputs=predictions)
     model.compile(loss='binary_crossentropy',
                   optimizer=tf.train.AdamOptimizer(0.001),
                   metrics=['binary_accuracy'])
     return model
 
+def train():
+    field_dict = {i:i//5 for i in range(30)}
+    ffm = FFM(30, field_dict, 6, 30)
+    data = load_breast_cancer()
+    X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, test_size=0.2,
+                                                        random_state=27, stratify=data.target)
+    ffm.fit(X_train, y_train, epochs=3, batch_size=16, validation_data=(X_test, y_test))
+    return ffm
 
+
+if __name__ == '__main__':
+    ffm = train()
